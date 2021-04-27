02b46254429441049bf69a73b8786d94fc0a3af3
student <student@ip-172-31-55-190.us-west-2.compute.internal>
2021-04-26 19:30:41 -0700

    finished

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,13 +5,15 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	int haplvl = 0;
+	static int pet, haplvl = 0;
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-		int pet = JOptionPane.showOptionDialog(null, "What pet do you want?", "Pop-up Title", 0, 
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Palamute", "Palico", "Cohoot", "FISH"}, null);
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		pet = JOptionPane.showOptionDialog(null, "What pet do you want?", "Pop-up Title", 0, 
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Palamute", "Palico", "Cohoot"}, null);
+		
+		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 5; i++) {
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
@@ -19,13 +21,63 @@ public class HappyPet {
 					new String[] { "Pet", "Hunt", "Feed" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			if (task == 0) {
+				pet();
+			} else if (task == 1) {
+				hunt();
+			} else if (task == 2) {
+				feed();
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+			if (haplvl >= 90) {
+				JOptionPane.showMessageDialog(null, "You love your pet.");
+				break;
+			}
+		}
 	}
-
+	
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	
+	static void pet() {
+		if (pet == 0) {
+			JOptionPane.showMessageDialog(null, "Your palamute wasn't excited.");
+			haplvl += 5;
+		} else if (pet == 1) {
+			JOptionPane.showMessageDialog(null, "Your palico purred.");
+			haplvl += 25;
+		} else if (pet == 2) {
+			JOptionPane.showMessageDialog(null, "Your cohoot ruffled its feathers.");
+			haplvl += 15;
+		}
+	}
+	
+	static void hunt() {
+		if (pet == 0) {
+			JOptionPane.showMessageDialog(null, "Your palamute is hyped up.");
+			haplvl += 25;
+		} else if (pet == 1) {
+			JOptionPane.showMessageDialog(null, "Your palico is prepared to hunt.");
+			haplvl += 15;
+		} else if (pet == 2) {
+			JOptionPane.showMessageDialog(null, "Your cohoot doesn't know how to hunt.");
+			haplvl += 5;
+		}
+	}
+	
+	static void feed() {
+		if (pet == 0) {
+			JOptionPane.showMessageDialog(null, "Your palamute is hungry after hunting.");
+			haplvl += 15;
+		} else if (pet == 1) {
+			JOptionPane.showMessageDialog(null, "Your palico isn't hungry.");
+			haplvl += 5;
+		} else if (pet == 2) {
+			JOptionPane.showMessageDialog(null, "Your cohoot loves seeds.");
+			haplvl += 25;
+		}
+	}
+	
 }
